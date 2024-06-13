cabe0774ab47df163d25e76ad10030253ea14ba2
Mootassam <Mootassame@gmail.com>
2024-06-13 18:18:50 +0100

    add the urls about the end

---
+++ admin/src/modules/shared/axios/authAxios.tsx
@@ -0,0 +1,51 @@
+import { AuthToken } from 'src/modules/auth/authToken';
+import Axios from 'axios';
+// import config from 'src/config'; 
+import { getLanguageCode } from 'src/i18n';
+import Qs from 'qs';
+import moment from 'moment';
+
+const authAxios = Axios.create({
+  // Louis link
+  // baseURL: 'http://139.162.155.71:8080/api', 
+  
+  // End Link 
+  baseURL: "http://172.104.141.32:8080/api",
+
+  //Local link
+  // baseURL: 'http://192.168.1.43:8080/api',  
+
+  
+  paramsSerializer: function (params) {
+    return Qs.stringify(params, {
+      arrayFormat: 'brackets',
+      filter: (prefix, value) => {
+        if (
+          moment.isMoment(value) ||
+          value instanceof Date
+        ) {
+          return value.toISOString();
+        }
+        return value;
+      },
+    });
+  },
+});
+
+authAxios.interceptors.request.use(
+  async function (options) {
+    const token = AuthToken.get();
+    if (token) {
+      options.headers['Authorization'] = `Bearer ${token}`;
+    }
+    options.headers['ngrok-skip-browser-warning'] = 'true';
+    options.headers['Accept-Language'] = getLanguageCode();
+    return options;
+  },
+  function (error) {
+    console.log('Request error: ', error);
+    return Promise.reject(error);
+  },
+);
+
+export default authAxios;
---
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import authToken from "src/modules/auth/authToken";
+
+const authAxios = axios.create({
+
+  // Local link
+  // baseURL: "http://localhost:8080/api",
+
+  // louis Link
+  // baseURL: "https://www.eclicks-digital.xyz/api/",
+
+  // ENd LInk
+  baseURL: "http://172.104.141.32:8080/api",
+
+});
+
+authAxios.interceptors.request.use(async function (options) {
+  const token = authToken.get();
+  if (token) {
+    options.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return options;
+});
+
+export default authAxios;
